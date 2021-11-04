295a7bf698fbb217732303ff42ae0dff0ce5718c
DzungVu <dzungvucs@gmail.com>
2021-11-04 17:23:57 +0700

    Compact message

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
         while True:
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(f"{data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast(f"Client #{client_id} left the chat")
