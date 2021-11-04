0b18a1c1f50f88eac0be8066685e29947034c25c
DzungVu <dzungvucs@gmail.com>
2021-11-04 14:49:34 +0700

    Update use 1 channel

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var client_id = Date.now()
+            var client_id = 'chatroom'
             document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`wss://host-socket.herokuapp.com/ws/chat`);
+            var ws = new WebSocket(`wss://host-socket.herokuapp.com/ws/${client_id}`);
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
